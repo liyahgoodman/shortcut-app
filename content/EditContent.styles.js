aaf85c4bc76dfba6080361e8c6ffde711e73bca7
liyahgoodman <aaliyah@perfectsensedigital.com>
2022-03-25 08:27:23 -0400

    Fix duplicate copied batch

--- content/EditContent.styles.js
+++
@@ -1,8 +0,0 @@
-import { css } from 'lit-element'
-
-export const EditContentStyles = css`
-  .edit-content {
-    padding: 1px 30px;
-    padding-bottom: 25px;
-  }
-`
--- search/SearchResults.styles.js
+++
@@ -1,7 +0,0 @@
-import { css } from 'lit-element'
-
-export const SearchResultsStyles = css`
-  .page-content {
-    padding: 20px;
-  }
-`
